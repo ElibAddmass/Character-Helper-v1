c1933330e6df39a53f2d326698c9c28d96ba842d
Elib Addmass <eliabadams2@gmail.com>
2025-10-27 20:56:24 -0400

    added importing files ability in index.js. char.json template for character.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,34 @@
-console.log('index.js loaded');
+const fr = new FileReader();
+const display = document.querySelector('.display');
+let importedChar = {}; 
+fr.addEventListener('load', (e) => {
+  const readFile = fr.result;
+  console.log(typeof readFile + `\n` + readFile);
+  if (readFile.includes('data:image')) {
+    display.innerHTML += `<img src="${readFile}">`;
+  } else {
+    importedChar = JSON.parse(readFile);
+    display.innerHTML += `<div>${readFile}</div>`;
+  }
+  console.log(importedChar);
+  
+});
+
+const loadCharInput = document.querySelector('.char-input-js');
+loadCharInput.addEventListener('change', (e) => {
+  const file = loadCharInput.files[0];
+  console.log(`file.type is ${file.type}`);
+  
+  if (!file) console.warn(`file is ${file}`);
+  else if (file.type.includes('image')) {
+    fr.readAsDataURL(file);
+  }
+  else if (file.type.includes('text') || file.type.includes('json')) {
+    fr.readAsText(file);
+  }
+  else {
+    console.warn('Unkown File Type. Please Try Something Else.');
+  }
+});
+
+
