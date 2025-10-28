47a3a92236107c43dafc7a8f6bdd0c0a01a78e9f
Elib Addmass <eliabadams2@gmail.com>
2025-10-28 13:42:37 -0400

    generate a nice index page with two buttons. One for creating new character, and one for importing a character

---
+++ scripts/index-ai.js
@@ -0,0 +1,9 @@
+document.querySelector('.create-char-js').addEventListener('click', () => {
+  console.log('create character');
+  
+});
+
+document.querySelector('.import-char-js').addEventListener('click', () => {
+  console.log('import character');
+  
+});
